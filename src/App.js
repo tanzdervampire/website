c2a78b8ab2b9cd548bf9ac523c5dd49acf1a2213
Ingo Bürk <ingo.buerk@tngtech.com>
2017-05-15 23:03:05 +0200

    Implement a dummy list

--- src/App.js
+++ src/App.js
@@ -11,6 +11,17 @@ import transitions from 'material-ui/styles/transitions';
 import ShowPicker from './ShowPicker/ShowPicker';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+
+import IconButton from 'material-ui/IconButton';
+import KeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
+import KeyboardArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
+
 import './App.css';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -123,23 +134,114 @@ class App extends Component {
         const styles = {
             content: {
                 backgroundColor: lightWhite
-            }
+            },
+            header: {
+                padding: 10,
+                marginBottom: 10,
+            },
+            listContainer: {
+                maxWidth: 500,
+                textAlign: "left",
+                position: "relative",
+                margin: "0 auto",
+            },
         };
 
         return (
             <div id="content-container" style={styles.content}>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
-                <p>Todo</p>
+                <div style={styles.listContainer}>
+                    <Paper>
+                        <Toolbar>
+                            <ToolbarGroup>
+                                <IconButton>
+                                    <KeyboardArrowLeft />
+                                </IconButton>
+                            </ToolbarGroup>
+                            <ToolbarGroup>
+                                <ToolbarTitle text="20.05.2017, Matinée, Stuttgart" />
+                            </ToolbarGroup>
+                            <ToolbarGroup>
+                                <IconButton>
+                                    <KeyboardArrowRight />
+                                </IconButton>
+                            </ToolbarGroup>
+                        </Toolbar>
+                        <List>
+                            <Subheader>Hauptrollen</Subheader>
+                            <ListItem
+                                disabled={true}
+                                primaryText="Jan Amann"
+                                secondaryText="Graf von Krolock"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Aris Sas"
+                                secondaryText="Alfred"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Victor Petersen"
+                                secondaryText="Professor Abronsius"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Veronica Appeddu"
+                                secondaryText="Sarah"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Nicolas Tenerani"
+                                secondaryText="Chagal"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Franziska Forster"
+                                secondaryText="Magda"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Simone Pohl"
+                                secondaryText="Rebecca"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Milan van Waardenburg"
+                                secondaryText="Herbert"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Paolo Bianca"
+                                secondaryText="Koukol"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <Divider />
+                            <Subheader>Tanzensemble</Subheader>
+                            <ListItem
+                                disabled={true}
+                                primaryText="Max Mustermann"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Max Mustermann"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                            <ListItem
+                                disabled={true}
+                                primaryText="Max Mustermann"
+                                leftAvatar={<Avatar src="images/avatar.jpg" />}
+                            />
+                        </List>
+                    </Paper>
+                </div>
             </div>
         );
     }
