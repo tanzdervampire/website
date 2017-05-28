4f05e7c9f97639b71e1f0c11ffd637f96f90b99d
Ingo Bürk <ingo.buerk@tngtech.com>
2017-05-28 07:28:05 +0200

    updated to newest react-scripts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 // @flow
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 import './index.css';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
     (
